603e9da009f9ac6c0492a1a383686edd865f6326
yalin:turtle [ubuntu-22.04] <yalindemir@outlook.de>
2025-07-09 22:41:23 +0200

    adding new world

---
+++ launch/start_px4.launch.py
@@ -0,0 +1,57 @@
+
+
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+import os
+
+def generate_launch_description():
+
+    # Dizinler - kişiselleştirilmelidir
+    px4_dir = os.path.expanduser('~/PX4-Autopilot')
+    agent_dir = os.path.expanduser('~/Micro-XRCE-DDS-Agent')
+    agent_port = '8888'
+
+    # Micro XRCE-DDS Agent başlat
+    microxrce_agent = ExecuteProcess(
+        cmd=["MicroXRCEAgent", "udp4", "-p", agent_port],
+        cwd=agent_dir,
+        output="screen"
+    )
+
+    # PX4 SITL + Gazebo başlat
+    px4_gz = ExecuteProcess(
+        cmd=["make", "px4_sitl", "gz_x500_depth"],
+        cwd=px4_dir,
+        output="screen",
+        additional_env={"PX4_GZ_WORLD": "walls"}
+    )
+
+    # (İsteğe Bağlı) ROS <-> Gazebo köprüsü örneği
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/camera@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+            '/depth_camera@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/depth_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked',
+        ],
+        output='screen'
+    )
+
+    offboard_controller_node = Node(
+        package='offboard_control',
+        executable='offboard_control_node',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        microxrce_agent,
+        px4_gz,
+        ros_gz_bridge,
+        offboard_controller_node
+    ])
